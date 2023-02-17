9163a2c40d2709f38cdefcfdc304c1b755e287b8
dsad2221s <ghksgml0555@naver.com>
2023-02-17 17:02:02 +0900

    서블릿으로 예외마다 오류페이지 배정
    서블릿으로 예외마다 오류페이지 배정

--- exception/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -1,4 +1,24 @@
 package hello.exception;
 
-public class WebServerCustomizer {
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+//WebServerFactoryCustomizer<ConfigurableWebServerFactory>를 구현해야한다.
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory){
+
+        //NOT_FOUND(404)예외가 발생하면 /error-page/400으로 호출된다.
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        //INTERNAL_SERVER_ERROR(500)예외가 발생하면 /error-page/500으로 호출된다.
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        //RuntimeException예외가 발생하면 /error-page/500으로 호출된다.
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
 }
