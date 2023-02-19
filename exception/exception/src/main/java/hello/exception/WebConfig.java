a1036f5155243a8ff19e8179e35474329cd8cc46
dsad2221s <ghksgml0555@naver.com>
2023-02-19 21:31:27 +0900

    필터, 인터셉터가 에러페이지뜰때 적용되지 않게 하기
    필터 > setDispatcherTypes로 적용될 디스패처 타입정하기
    
    인터셉터 > excluedePathPatterns로 뺄 페이지들 적용

---
+++ exception/exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,40 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+                /*인터셉터는 필터처럼 디스패처타입등을 세팅할 수 없다.
+                   대신에 강력한 excluedPathPatterns로 에러페이지 관련된걸 모두 뺀다
+                   ( "/error-page/**" )
+                 */
+    }
+
+  //  @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new
+                FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        //이 필터는 request, error두가지 타입에 호출된다.
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        return filterRegistrationBean;
+    }
+}
