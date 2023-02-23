15844b04297a1bd344bc6eac43552f74f8ac88fe
dsad2221s <ghksgml0555@naver.com>
2023-02-23 19:13:38 +0900

    포매터를 웹애플리케이션에 적용
    포매터를 웹애플리케이션에 적용

--- typeconverter/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistrar;
 import org.springframework.format.FormatterRegistry;
@@ -14,9 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry){
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //우선순위 때문에 주석처리한다.(같은기능일때 컨버터가 우선순위가 높다)
+    //    registry.addConverter(new StringToIntegerConverter());
+    //    registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
